6fbdbe1940b10b6ceb42f993a6c8603f19ad2852
Kropochev Aleksandr <kropochev.a@gmail.com>
2022-11-27 23:59:06 +0500

    Add error messages

--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import User
@@ -28,6 +28,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрированы!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
