9e9a254653245a3142cedea146afc7da84f819d1
Kropochev Aleksandr <kropochev.a@gmail.com>
2022-11-28 00:12:09 +0500

    Add logout

--- store/users/views.py
+++ store/users/views.py
@@ -52,3 +52,8 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
