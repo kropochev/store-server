b179f45d0f01f2ff17ab6ba163f45fc637d8838b
Kropochev Aleksandr <kropochev.a@gmail.com>
2022-12-10 12:46:59 +0500

    Add filtration

--- store/products/views.py
+++ store/products/views.py
@@ -11,10 +11,15 @@ def index(request):
     return render(request, "products/index.html", context)
 
 
-def products(request):
+def products(request, category_id=None):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
     context = {
         "title": "Store - Каталог",
-        'products': Product.objects.all(),
+        'products': products,
         'categories': ProductCategory.objects.all(),
     }
     return render(request, "products/products.html", context)
