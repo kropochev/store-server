cad8dba0b505f97a4d9f43ab97f776fc332ebadf
Kropochev Aleksandr <kropochev.a@gmail.com>
2022-12-11 22:49:42 +0500

    Refactoring

--- store/users/forms.py
+++ store/users/forms.py
@@ -2,99 +2,48 @@ import uuid
 from datetime import timedelta
 
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 from django.utils.timezone import now
-from django.contrib.auth.forms import (
-    AuthenticationForm,
-    UserCreationForm,
-    UserChangeForm,
-)
 
-from users.models import User, EmailVerification
+from users.models import EmailVerification, User
 
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите имя пользователя",
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите пароль"
-            }
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите пароль'})
     )
 
     class Meta:
         model = User
-        fields = ("username", "password")
+        fields = ('username', 'password')
 
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите имя",
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя'})
     )
     last_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите фамилию",
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите фамилию'})
     )
     username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите имя пользователя",
-            }
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'})
     )
     email = forms.CharField(
-        widget=forms.EmailInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите адрес эл. почты",
-            }
-        )
+        widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'})
     )
     password1 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Введите пароль",
-            }
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите пароль'})
     )
     password2 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "class": "form-control py-4",
-                "placeholder": "Подтвердите пароль",
-            }
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'})
     )
 
     class Meta:
         model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "username",
-            "email",
-            "password1",
-            "password2",
-        )
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super(UserRegistrationForm, self).save(commit=True)
@@ -107,51 +56,12 @@ class UserRegistrationForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-            }
-        )
-    )
-    last_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-            }
-        )
-    )
-    image = forms.ImageField(
-        widget=forms.FileInput(
-            attrs={
-                "class": "custom-file-input",
-            }
-        ),
-        required=False,
-    )
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "class": "form-control py-4",
-                "readonly": True,
-            }
-        )
-    )
-    email = forms.CharField(
-        widget=forms.EmailInput(
-            attrs={
-                "class": "form-control py-4",
-                "readonly": True,
-            }
-        )
-    )
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': True}))
 
     class Meta:
         model = User
-        fields = (
-            "first_name",
-            "last_name",
-            "image",
-            "username",
-            "email",
-        )
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
--- store/users/views.py
+++ store/users/views.py
@@ -1,13 +1,13 @@
-from django.urls import reverse, reverse_lazy
-from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
-from django.contrib.messages.views import SuccessMessageMixin
 
 from common.views import TitleMixin
-from users.models import User, EmailVerification
-from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
+from users.models import EmailVerification, User
 
 
 class UserLoginView(TitleMixin, LoginView):
